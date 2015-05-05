66a8ede5068bc618bd8ec42d580e95304f64cade
trikitrok <trikitrok@gmail.com>
2015-05-05 12:40:19 +0200

    Extracted some helpers and renamed some functions and parameters

--- src/string_calculator/core.clj
+++ src/string_calculator/core.clj
@@ -1,21 +1,29 @@
 (ns string-calculator.core)
 
+(def default-delimiters ["," "\\n"])
+
+(defn create-delimiters-pattern [given-delimiters]
+  (re-pattern
+    (clojure.string/join
+      "|"
+      (concat default-delimiters given-delimiters))))
+
 (defn extract-delimiters-and-numbers [input]
   (if-let [delimiters-and-numbers (first (re-seq #"//(.+)\n(.*)" input))]
     (drop 1 delimiters-and-numbers)
     ["" input]))
 
-(defn parse-numbers [nums-str]
-  (let [[given-delimiters nums-str] (extract-delimiters-and-numbers nums-str)
-        delimiters (concat ["," "\\n"] given-delimiters)
-        delimiters-pattern (clojure.string/join "|" delimiters)]
-    (clojure.string/split nums-str (re-pattern delimiters-pattern))))
+(defn extract-nums-str [input-str]
+  (let [[given-delimiters nums-str] (extract-delimiters-and-numbers input-str)]
+    (clojure.string/split
+      nums-str
+      (create-delimiters-pattern given-delimiters))))
 
-(defn extract-numbers [nums-str]
-  (if (empty? nums-str)
+(defn parse-numbers [input-str]
+  (if (empty? input-str)
     [0]
     (map #(Integer/parseInt %)
-         (parse-numbers nums-str))))
+         (extract-nums-str input-str))))
 
-(defn add [nums-str]
-  (apply + (extract-numbers nums-str)))
+(defn add [input-str]
+  (apply + (parse-numbers input-str)))

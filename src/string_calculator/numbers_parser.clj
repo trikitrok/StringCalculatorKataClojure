ca8d5d5ea14c686fdac1162b312342cbd6b1430d
trikitrok <trikitrok@gmail.com>
2015-05-05 19:52:05 +0200

    Modified using Francesc Guillen's simplification

--- src/string_calculator/numbers_parser.clj
+++ src/string_calculator/numbers_parser.clj
@@ -1,46 +1,13 @@
-(ns string-calculator.numbers-parser
-  (:require [clojure.string :as string]))
-
-(def ^:private default-delimiters ["," "\n"])
-
-(def ^:private escaped-chars-by-metachar
-  (let [esc-chars "()*&^%$#!"]
-    (zipmap esc-chars
-            (map #(str "\\" %) esc-chars))))
-
-(defn- escape-meta-characters [delimiters-str]
-  (reduce str (map #(get escaped-chars-by-metachar % %)
-                   delimiters-str)))
+(ns string-calculator.numbers-parser)
 
 (defn- get-matches [pattern string]
   (mapcat (partial drop 1) (re-seq pattern string)))
 
-(defn- extract-delimiters [delimiters-str]
-  (let [delimiters (get-matches #"\[(.*?)\]" delimiters-str)]
-    (if (empty? delimiters)
-      delimiters-str
-      delimiters)))
+(defn str->int [str-num]
+  (Integer/parseInt str-num))
 
-(defn- create-delimiters-pattern [delimiters-str]
-  (->> delimiters-str
-       extract-delimiters
-       (concat default-delimiters)
-       (string/join "|")
-       escape-meta-characters
-       re-pattern))
-
-(defn- numbers-and-delimiters-pattern [input]
-  (let [delimiters-and-numbers (get-matches #"//(.+)\n(.*)" input)]
-    [(or (second delimiters-and-numbers) input)
-     (create-delimiters-pattern
-       (or (first delimiters-and-numbers) ""))]))
-
-(defn- extract-nums-str [input-str]
-  (apply string/split
-         (numbers-and-delimiters-pattern input-str)))
-
-(defn parse [input-str]
-  (if (string/blank? input-str)
-    [0]
-    (map #(Integer/parseInt %)
-         (extract-nums-str input-str))))
+(defn parse [str-cmd]
+  (let [nums-str (get-matches #"([-\d]+)" str-cmd)]
+    (if (empty? nums-str)
+      [0]
+      (map str->int nums-str))))

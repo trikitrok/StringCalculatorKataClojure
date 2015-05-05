6c5bbe8a8ed7f0b9185c56b9e300a84d39328b82
trikitrok <trikitrok@gmail.com>
2015-05-05 16:26:40 +0200

    Moved some responsibilities

--- src/string_calculator/numbers_parser.clj
+++ src/string_calculator/numbers_parser.clj
@@ -11,28 +11,30 @@
   (reduce str (map #(get escaped-chars-by-matachar % %)
                    delimiters-str)))
 
-(defn- create-delimiters-pattern [given-delimiters]
-  (re-pattern
-    (escape-meta-characters
-      (clojure.string/join "|" (concat default-delimiters given-delimiters)))))
-
 (def ^:private get-matches (partial drop 1))
-
-(defn- extract-delimiters-and-numbers [input]
-  (if-let [delimiters-and-numbers (re-seq #"//(.+)\n(.*)" input)]
-    (mapcat get-matches delimiters-and-numbers)
-    ["" input]))
 
 (defn- extract-delimiters [delimiters-str]
   (if-let [delimiters (re-seq #"\[(.*?)\]" delimiters-str)]
     (mapcat get-matches delimiters)
     delimiters-str))
 
+(defn- create-delimiters-pattern [delimiters-str]
+  (re-pattern
+    (escape-meta-characters
+      (clojure.string/join "|"
+                           (concat default-delimiters
+                                   (extract-delimiters delimiters-str))))))
+
+(defn- extract-delimiters-and-numbers [input]
+  (if-let [delimiters-and-numbers (re-seq #"//(.+)\n(.*)" input)]
+    (mapcat get-matches delimiters-and-numbers)
+    ["" input]))
+
 (defn- extract-nums-str [input-str]
-  (let [[given-delimiters nums-str] (extract-delimiters-and-numbers input-str)]
+  (let [[delimiters-str nums-str] (extract-delimiters-and-numbers input-str)]
     (clojure.string/split
       nums-str
-      (create-delimiters-pattern (extract-delimiters given-delimiters)))))
+      (create-delimiters-pattern delimiters-str))))
 
 (defn parse [input-str]
   (if (empty? input-str)
